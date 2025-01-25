aeda9b860b1119d8eca2bde65eeb89de9fa3c915
Peter Stovka <stovka.peter@gmail.com>
2025-01-25 13:02:39 +0100

    wip

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use App\Models\Lesson;
+use App\View\Models\VideoSource;
 use Illuminate\Support\Facades\Gate;
 use Inertia\Inertia;
 
@@ -18,6 +19,9 @@ class LessonController
         return Inertia::render('Courses/CourseLesson', [
             'courseTitle' => $course->title,
             'lessonTitle' => $lesson->title,
+            'video' => VideoSource::for($lesson->video),
+            'description' => $lesson->description,
+            'resources' => [], // TODO: Add resources
         ]);
     }
 }
--- resources/js/Pages/Courses/CourseLesson.vue
+++ resources/js/Pages/Courses/CourseLesson.vue
@@ -9,8 +9,33 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="flex flex-row">
         <div class="flex-1 relative" style="height: calc(100vh - 8rem)">
-          <div class="absolute inset-0 flex flex-col overflow-y-auto">
-            <p>video</p>
+          <div class="absolute inset-0 flex flex-col overflow-y-auto py-6 pr-6">
+            <Player
+              v-if="video"
+              :src="video.url"
+              :poster="video.posterImageUrl"
+            />
+
+            <div class="flex flex-row justify-between items-center mt-6">
+              <h2 class="font-semibold text-xl leading-tight">{{ lessonTitle }}</h2>
+
+              <div class="inline-flex flex-row gap-4">
+                <Button variant="outline">Mark Completed</Button>
+              </div>
+            </div>
+
+            <Tabs v-if="description || resources.length > 0" :default-value="description ? 'description' : 'resources'">
+              <TabsList class="mt-6">
+                <TabsTrigger v-if="description" value="description">Description</TabsTrigger>
+                <TabsTrigger v-if="resources.length > 0" value="resources">Resources</TabsTrigger>
+              </TabsList>
+              <TabsContent value="description" v-if="description">
+                <div class="editor-content" v-html="description"></div>
+              </TabsContent>
+              <TabsContent value="resources">
+                <p>resources</p>
+              </TabsContent>
+            </Tabs>
           </div>
         </div>
 
@@ -25,11 +50,18 @@
 </template>
 
 <script setup lang="ts">
+import { Player } from '@/Components/Player'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/Components/Tabs'
 import { AuthenticatedLayout } from '@/Layouts'
+import type { VideoSource } from '@/Types'
 import { Head } from '@inertiajs/vue3'
+import { Button } from '@/Components/Button'
 
 defineProps<{
   courseTitle: string
   lessonTitle: string
+  video: VideoSource | null
+  description: string | null
+  resources: Array<{}>
 }>()
 </script>
