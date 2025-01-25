3eefe89a308841a6605612b5310cf0acc8edd269
Peter Stovka <stovka.peter@gmail.com>
2025-01-25 22:02:10 +0100

    trust proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        $middleware->trustProxies('10.32.0.0/16');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
