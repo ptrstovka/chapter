1776a162c6e504f1428102c6f341ba1935dd0a63
Peter Stovka <stovka.peter@gmail.com>
2025-01-26 11:10:25 +0100

    wip

--- resources/js/Pages/Courses/CourseList.vue
+++ resources/js/Pages/Courses/CourseList.vue
@@ -52,34 +52,38 @@
             </div>
 
             <div v-if="courses.data.length > 0" class="grid grid-cols-3 my-6 gap-4 w-full">
-              <Link v-for="course in courses.data" :href="course.url" class="border w-full bg-background rounded-md overflow-hidden">
-                <div class="h-44 relative">
-                  <img :class="{ 'grayscale': course.enrollment && course.enrollment.isCompleted }" class="w-full h-full object-cover" v-if="course.coverImageUrl" :src="course.coverImageUrl" :alt="`Cover image of ${course.title} course`">
-                  <div v-else class="w-full h-full bg-stone-50 flex items-center justify-center">
-                    <ImageOffIcon class="w-5 h-5 text-stone-400" />
-                  </div>
+              <Card v-for="course in courses.data" class="p-0 overflow-hidden">
+                <CardContent class="p-0">
+                  <Link :href="course.url">
+                    <div class="h-44 relative">
+                      <img :class="{ 'grayscale': course.enrollment && course.enrollment.isCompleted }" class="w-full h-full object-cover" v-if="course.coverImageUrl" :src="course.coverImageUrl" :alt="`Cover image of ${course.title} course`">
+                      <div v-else class="w-full h-full bg-stone-50 flex items-center justify-center">
+                        <ImageOffIcon class="w-5 h-5 text-stone-400" />
+                      </div>
 
-                  <Badge v-if="course.enrollment && course.enrollment.isCompleted" class="absolute top-2 left-2 gap-0.5 pl-1.5" variant="positive">
-                    <CheckIcon class="w-4 h-4 mt-px" />
-                    Completed
-                  </Badge>
-                  <Badge v-if="course.enrollment && !course.enrollment.isCompleted" class="absolute top-2 left-2">{{ course.enrollment.progress }}% In Progress</Badge>
-                </div>
-                <div class="border-t px-2 py-3">
-                  <p class="font-semibold">{{ course.title }}</p>
+                      <Badge v-if="course.enrollment && course.enrollment.isCompleted" class="absolute top-2 left-2 gap-0.5 pl-1.5" variant="positive">
+                        <CheckIcon class="w-4 h-4 mt-px" />
+                        Completed
+                      </Badge>
+                      <Badge v-if="course.enrollment && !course.enrollment.isCompleted" class="absolute top-2 left-2">{{ course.enrollment.progress }}% In Progress</Badge>
+                    </div>
+                    <div class="border-t px-2 py-3">
+                      <p class="font-semibold">{{ course.title }}</p>
 
-                  <div class="flex flex-row mt-2 gap-2">
-                    <p class="text-xs font-medium text-muted-foreground inline-flex items-center gap-1">
-                      <ContactIcon class="w-4 h-4" />
-                      {{ course.author.name }}
-                    </p>
-                    <p v-if="course.duration" class="text-xs font-medium text-muted-foreground inline-flex tabular-nums items-center gap-1">
-                      <TimerIcon class="w-4 h-4" />
-                      {{ course.duration }}
-                    </p>
-                  </div>
-                </div>
-              </Link>
+                      <div class="flex flex-row mt-2 gap-2">
+                        <p class="text-xs font-medium text-muted-foreground inline-flex items-center gap-1">
+                          <ContactIcon class="w-4 h-4" />
+                          {{ course.author.name }}
+                        </p>
+                        <p v-if="course.duration" class="text-xs font-medium text-muted-foreground inline-flex tabular-nums items-center gap-1">
+                          <TimerIcon class="w-4 h-4" />
+                          {{ course.duration }}
+                        </p>
+                      </div>
+                    </div>
+                  </Link>
+                </CardContent>
+              </Card>
             </div>
 
             <div v-if="courses.total > 0" class="flex flex-row justify-end w-full">
@@ -105,6 +109,7 @@ import { SimplePagination } from '@/Components/Pagination'
 import { Badge } from '@/Components/Badge'
 import { CheckboxControl } from '@/Components/Checkbox'
 import EmptyPattern from './Components/EmptyPattern.vue'
+import { Card, CardContent } from '@/Components/Card'
 
 defineProps<{
   category: string | null
