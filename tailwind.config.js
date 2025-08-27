51d29c099b2f84eda64672eca5706cba9b06dc4f
Peter Stovka <stovka.peter@gmail.com>
2025-08-27 18:13:29 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,15 @@
-import vidstack from 'vidstack/tailwind.cjs'
-
 export default {
     plugins: [
-        vidstack({
-            prefix: 'media',
-            webComponents: true,
-        }),
-        function ({ addVariant, matchVariant }) {
-            // Strict version of `.group` to help with nesting.
-            matchVariant('parent-data', (value) => `.parent[data-${value}] > &`);
-
-            addVariant('hocus', ['&:hover', '&:focus-visible']);
-            addVariant('group-hocus', ['.group:hover &', '.group:focus-visible &']);
-        },
+        // vidstack({
+        //     prefix: 'media',
+        //     webComponents: true,
+        // }),
+        // function ({ addVariant, matchVariant }) {
+        //     // Strict version of `.group` to help with nesting.
+        //     matchVariant('parent-data', (value) => `.parent[data-${value}] > &`);
+        //
+        //     addVariant('hocus', ['&:hover', '&:focus-visible']);
+        //     addVariant('group-hocus', ['.group:hover &', '.group:focus-visible &']);
+        // },
     ]
 }
